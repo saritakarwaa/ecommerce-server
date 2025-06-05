0f18b2d37d5c6b20647f2cb9a0bfccb3dda5ca43
saritakarwaa <karwasarita672@gmail.com>
2025-06-05 17:07:08 +0530

    added order routes

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -11,18 +11,14 @@ import { authenticate } from '../middlewares/auth';
 
 const router = express.Router();
 
-// User
 router.post('/', authenticate(['user']), createOrder as express.RequestHandler);
 router.get('/user', authenticate(['user']), getUserOrders);
 
-// Seller
 router.put('/:id/status', authenticate(['seller']), updateOrderStatus as express.RequestHandler);
 router.get('/seller', authenticate(['seller']), getSellerOrders);
 
-// Shared
 router.get('/:id', authenticate(['user', 'seller']), getOrderById as express.RequestHandler);
 
-// Admin
 router.get('/', authenticate(['admin','superadmin']), getAllOrders);
 
 export default router;
